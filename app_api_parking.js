3616451abf34337bfc0329e21cfcd3394e415df2
AlexF <84327508+AlexandreFigard@users.noreply.github.com>
2021-06-22 10:05:51 +0200

    Update app_api_parking.js

--- app_api_parking.js
+++ app_api_parking.js
@@ -13,6 +13,7 @@ app.get('/parkings', (req,res) => {
  res.status(200).json(parkings);
 })
 
+
 // route id parkings
 app.get('/parkings/:id', (req,res) => {
     const id = parseInt(req.params.id); // Convertion en nombre
